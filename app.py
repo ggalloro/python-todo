7818a1d04fd6129a33f17b827851370bd7e19128
ggalloro <galloro.giovanni@gmail.com>
2022-01-06 20:31:56 +0100

    Real 1st commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+class AddTask(FlaskForm):
+    task = StringField("Task", validators = [DataRequired()])
+    submit = SubmitField("Aggiungi Task")
+
+
+todos = ["Fai un app per ristoranti","Deploya su Cloud run"]
+
+@app.route('/', methods = ["GET","POST"])
+def index():
+    add_task = AddTask()
+    if add_task.validate_on_submit():
+        todos.append(add_task.task.data)
+        return redirect('/')
+    return render_template("index.html", todos = todos, add_task=add_task)
+    
+
+
+
